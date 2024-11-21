fc030b5175e18dee0fe7929473374e3ccee7d5b0
Ricardo Vargas <ricardovm2803@gmail.com>
2024-11-21 15:54:02 -0600

    Se agrega listado de pacientes

---
+++ BoenaCare/Controllers/PacientesController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BoenaCare.Controllers
+{
+    public class PacientesController : Controller
+    {
+        [HttpGet]
+        public ActionResult Listado()
+        {
+            return View();
+        }
+
+    }
+}
