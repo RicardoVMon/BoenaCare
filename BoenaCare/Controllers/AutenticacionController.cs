444b9545c70dc321ca96e68b876a8448f6ec799f
Ricardo Vargas <ricardovm2803@gmail.com>
2024-11-20 21:44:07 -0600

    Add project files.

---
+++ BoenaCare/Controllers/AutenticacionController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BoenaCare.Controllers
+{
+    public class AutenticacionController : Controller
+    {
+        [HttpGet]
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult RecuperarContrasennia()
+        {
+            return View();
+        }
+    }
+}
