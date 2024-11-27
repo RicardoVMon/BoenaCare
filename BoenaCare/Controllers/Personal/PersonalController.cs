4023f13510cacc7106e21248b3cf42e69ce63497
Daniel Vega Marvez <87838943+Chuz047@users.noreply.github.com>
2024-11-26 18:13:00 -0600

    Vistas de perfil para ambos roles. Organización de la estructura del proyecto. Conexiones de controladores y vistas

---
+++ BoenaCare/Controllers/Personal/PersonalController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BoenaCare.Controllers
+{
+    public class PersonalController : Controller
+    {
+        // Funcionalidad: Perfil
+        public ActionResult Perfil()
+        {
+            return View();
+        }
+
+        public ActionResult ConfigurarPerfil()
+        {
+            return View();
+        }
+
+        // Funcionalidad: Inicio
+        public ActionResult Inicio()
+        {
+            return View();
+        }
+
+        // Funcionalidad: Pacientes
+        public ActionResult Listado()
+        {
+            return View();
+        }
+
+        // Funcionalidad: Archivos 
+        public ActionResult Archivos()
+        {
+            return View();
+        }
+    }
+}
